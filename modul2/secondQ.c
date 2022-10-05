ef11f398a0e04452e6ff2c30ad16266507dec177
Denta Hidayat <denta.dent53@gmail.com>
2022-10-05 08:48:36 +0700

    q2

--- modul2/secondQ.c
+++ modul2/secondQ.c
@@ -10,6 +10,31 @@
 #include <math.h>
 
 int main() {
-
+    int n, count = 0, index;
+    int check[100] = {false};
+    int box[100] = {0};
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &box[i]);
+        // printf("box=%d ", box[i]);
+    }
+    for (int j = 0; j < n; j++)
+    {
+        index = j;
+        if (check[j]==false)
+        {
+            count++;
+        }
+        
+        while (check[index] == false)
+        {
+            check[index] = true;
+            index = box[index];
+        }
+        
+    }
+    printf("%d \n", count);
+    
     return 0;
 }
